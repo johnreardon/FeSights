00c4cd67323b20e9fd07f827653bc1b340b58da2
John Reardon <jjr@corp.mpay.com>
2016-10-14 12:35:11 -0400

    moved main thread code to src.  Started adding a menu

--- src/main.js
+++ src/main.js
@@ -1,23 +1,52 @@
 'use strict';
 
 const electron  = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow, ipc, Menu} = electron;
 
 require('electron-reload')(__dirname);
 let mainWindow = null;
 
-function createWindow() 
+function createWindow()
 {
 	mainWindow = new BrowserWindow({
 		width:800,
 		height:600
 	});
+
+	const name = electron.app.getName();
+	const menuTemplate = [
+		{
+			label: "File",
+			submenu: [
+				{
+					label: `About ${name}`,
+					click: () => {
+						console.log( "about clicked");
+					}
+				},
+				{
+					type: "separator"
+				},
+				{
+					label: "Quit",
+					click: () => {
+						console.log( "Exiting");
+						app.quit();
+					},
+					accelerator: 'Alt+F4'
+				}
+			]
+		}
+	]
+	const menu = Menu.buildFromTemplate( menuTemplate);
+	Menu.setApplicationMenu(menu);
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 	mainWindow.webContents.openDevTools();
 	mainWindow.on( 'closed', () => {
 		mainWindow = null;
 	});
+
+
 }
 
 app.on( 'ready', createWindow);
